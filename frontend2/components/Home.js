83f3caeda7699e53b17d168ebb52d32a84606c32
Faris Durrani <fdurrani@mythics.com>
2023-02-11 16:37:28 -0500

    react-native

---
+++ frontend2/components/Home.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
